29393838130dbbe375be15662226cbbd449d5deb
durga.p <durga.p@flipkart.com>
2015-01-24 17:47:12 +0530

    loading actors from tiled

---
+++ core/assets/tiledMaps/actors.tsx
@@ -0,0 +1,29 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="actors" tilewidth="70" tileheight="70">
+ <image source="../packed/actors.png" width="512" height="128"/>
+ <tile id="0">
+  <properties>
+   <property name="type" value="enemy"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="type" value="hero"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="type" value="boss"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="enemy2" value=""/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="type" value="lava"/>
+  </properties>
+ </tile>
+</tileset>
