11723b988e2e064d73c1061aed28db83d2778091
Sudheer Kumar <sudheer667@gmail.com>
2015-01-25 00:36:12 +0530

    actor type

--- core/src/com/mygdx/game/ActorType.java
+++ core/src/com/mygdx/game/ActorType.java
@@ -4,7 +4,13 @@ package com.mygdx.game;
  * Created by durga.p on 1/24/15.
  */
 public enum ActorType {
-    OLD_MAN("actors/oldman.png");
+    OLD_MAN("actors/oldman.png"),
+    SWORD("actors/sword.png"),
+    SHIELD("actors/shield.png"),
+    BOW("actors/bow.png"),
+    AURA("actors/aura.png"),
+    LAVA("actors/lava.png");
+
     public final String fileName;
 
     ActorType(String s) {
